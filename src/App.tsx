c3c91dab4d4ff326dca55fd498925307166c5b21
Deeokafor <okafor.udahdivine@gmail.com>
2025-08-30 14:31:31 +0100

    fixed broken link to background image in main

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <main className="tw:text-center tw:w-full tw:h-min-screen tw:h-screen tw:flex tw:justify-center tw:font-josefin-sans tw:text-todo-lt-dark-grayish-blue">
-        <div className="tw:bg-[url(public/bg-mobile-light.jpg)] tw:w-full tw:h-[300px] tw:bg-no-repeat tw:bg-cover tw:fixed tw:-z-20"></div>
+        <div className="tw:bg-[url(bg-mobile-light.jpg)] tw:w-full tw:h-[300px] tw:bg-no-repeat tw:bg-cover tw:fixed tw:-z-20"></div>
 
         <div className='tw:w-[80%] tw:font-semibold tw:text tw:pb-14'>
           <TodoHeader />
