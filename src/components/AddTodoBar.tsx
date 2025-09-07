9cc3cec7141ddaa9e08f8c14ceb2274fab9ec24b
Deeokafor <okafor.udahdivine@gmail.com>
2025-09-07 23:28:26 +0100

    added json file and wrote function to update ui with json data

--- src/components/AddTodoBar.tsx
+++ src/components/AddTodoBar.tsx
@@ -1,9 +1,55 @@
-function AddTodoBar() {
+import React, { useState } from 'react';
+import data from '../assets/data.json'; // Import the JSON data
+
+interface TodoItem {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+function AddTodoBar({ onTodoAdded }: { onTodoAdded: (newTodo: TodoItem) => void }) {
+    const [newTodoText, setNewTodoText] = useState('');
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewTodoText(e.target.value);
+    };
+
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && newTodoText.trim() !== '') {
+            // Determine the next available ID
+            const maxId = data.reduce((max, todo) => Math.max(max, todo.id), 0);
+            const newId = maxId + 1;
+
+            const newTodo: TodoItem = {
+                id: newId,
+                text: newTodoText.trim(),
+                completed: false,
+            };
+
+            onTodoAdded(newTodo);
+            setNewTodoText(''); // Clear the input field
+        }
+    };
+
     return (
         <>
             <div className="tw:w-full tw:px-5 tw:py-5 tw:bg-white tw:flex tw:rounded-lg tw:mt-10">
-                <input type="radio" name="addTodo" id="" className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1"/>
-                <input type="text" name="" id="" placeholder="Create a new todo..." className="tw:focus:text-todo-lt-very-dark-grayish-blue tw:focus:outline-hidden"/>
+                <input
+                    type="radio"
+                    name="addTodo"
+                    id=""
+                    className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1"
+                />
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Create a new todo..."
+                    className="tw:focus:text-todo-lt-very-dark-grayish-blue tw:focus:outline-hidden"
+                    value={newTodoText}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                />
             </div>
         </>
     );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,41 +1,48 @@
+import data from "../assets/data.json";
+
+interface TodoItem {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+
 function TodoList() {
+    const itemsLeft = data.filter(item => !item.completed).length;
+
     return (
         <>
-            <div className="tw:mt-6 tw:rounded-lg tw:bg-white tw:shadow-xl tw:text-todo-lt-very-dark-grayish-blue tw:font-black">
-                <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]">
-                    <input className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1 tw:checked:hidden" type="checkbox" name="" id="" />
-                    <span className="tw:mr-auto">take out bin</span>
-                    <span className="close"><img src="icon-cross.svg" alt=""/></span>
-                </div>
-                <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]">
-                    <input className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1" type="checkbox" name="" id="" />
-                    <span className="tw:mr-auto">take out bin</span>
-                    <span className="close"><img src="icon-cross.svg" alt=""/></span>
-                </div>
-                <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]">
-                    <input className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1" type="checkbox" name="" id="" />
-                    <span className="tw:mr-auto">take out bin</span>
-                    <span className="close"><img src="icon-cross.svg" alt=""/></span>
-                </div>
-                <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]">
-                    <input className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1" type="checkbox" name="" id="" />
-                    <span className="tw:mr-auto">take out bin</span>
-                    <span className="close"><img src="icon-cross.svg" alt=""/></span>
-                </div>
-                <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]">
-                    <input className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1" type="checkbox" name="" id="" />
-                    <span className="tw:mr-auto">take out bin</span>
-                    <span className="close"><img src="icon-cross.svg" alt=""/></span>
-                </div>
-                
+            <ul className="tw:mt-6 tw:rounded-lg tw:bg-white tw:shadow-xl tw:text-todo-lt-very-dark-grayish-blue tw:font-black">
+                {data.map((item: TodoItem) => (
+                    <li
+                        key={item.id}
+                        className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:border-todo-lt-dark-grayish-blue/35 tw:border-b-[1.5px]"
+                    >
+                        <input
+                            className="tw:mr-3 tw:appearance-none tw:after:border-[2px] tw:after:rounded-full tw:after:border-todo-lt-light-grayish-blue tw:after:px-3 tw:after:py-1 tw:checked:hidden"
+                            type="checkbox"
+                            checked={item.completed}
+                            name=""
+                            id=""
+                        />
+                        <span className="tw:mr-auto">{item.text}</span>
+                        <span className="close">
+                            <img src="icon-cross.svg" alt="" />
+                        </span>
+                    </li>
+                ))}
 
                 <div className="tw:w-full tw:px-5 tw:py-5 tw:flex tw:flex-row tw:justify-between tw:text-todo-lt-dark-grayish-blue">
-                    <p><span>0</span> items left</p>
-                    <button id="clearCompletedItems" className="tw:capitalize" type="button">clear completed</button>
+                    <p>
+                        <span>{itemsLeft}</span> items left
+                    </p>
+                    <button id="clearCompletedItems" className="tw:capitalize" type="button">
+                        clear completed
+                    </button>
                 </div>
-            </div>
+            </ul>
         </>
-    )
+    );
 }
 
 export { TodoList };
